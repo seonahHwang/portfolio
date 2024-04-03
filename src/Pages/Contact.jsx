0441d660daf702b542a680d6b6c94d9ee8e5f5c5
seonahHwang <tjsdk2769@gmail.com>
2024-04-02 22:34:20 -0600

    contact

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -40,7 +40,7 @@ const Contact = () => {
         <SocialLinks />
         <div className="bg-grayscale-950 flex justify-center pb-4 text-grayscale-50">
           <span>
-            Made by <em>Sara Echeverría (bl33h)</em>
+            {/*Made by <em>Sara Echeverría (bl33h)</em>*/}
           </span>
         </div>
       </div>
